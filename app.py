396cba03f5bb372fdd7d73c8570610cde0c5bd80
Alifia Mustika Sari <111949698+alflouvre@users.noreply.github.com>
2022-12-16 15:49:20 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,30 +1,26 @@
 import numpy as np
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, render_template, request, jsonify
 import pickle
 import math
 
-app = Flask(__name__, template_folder='templates')
-model = pickle.load(open('decision_tree.pkl','rb'))
+app = Flask(__name__)
+model = pickle.load(open("model.pkl", "rb"))
 
-
-@app.route('/')
+@app.route("/")
 def home():
-    
     return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
+@app.route("/predict", methods=['POST'])
 def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    int_features  = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
-    
-    output = round(prediction[0],2)
-    
-    return render_template('index.html',prediction_text="Price Should be {}".format(output[prediction[0]]))
+    float_features = [float(x) for x in request.form.values()]
+    feature = [np.array(float_features)]
+    prediction = model.predict(feature)
+    #output = round(prediction[0],2)
+    #output = {0: 'tidak terkena gagal jantung', 1:'terkena gagal jantung'}
+    #output = round(prediction[0],2)
+    #return render_template("index.html", prediction_text = "Pasien {}".format(output[prediction[0]]))
+    return render_template("index.html", prediction_text = "${}".format(prediction))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
